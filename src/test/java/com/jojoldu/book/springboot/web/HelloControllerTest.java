e592496374b87ca296c409b06b5a048c2ce63dea
ej <clignancourt64@gmail.com>
2020-01-17 15:03:27 +0900

    commit before connecting ec2

--- src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -13,16 +13,14 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class,
-    excludeFilters = {
-        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,
-            classes = SecurityConfig.class)
-    })
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,
+                        classes = SecurityConfig.class)
+        })
 public class HelloControllerTest {
 
     @Autowired
@@ -34,10 +32,10 @@ public class HelloControllerTest {
         String hello = "hello";
 
         mvc.perform(get("/hello"))
-           .andExpect(status().isOk()).andExpect(content().string(hello));
+                .andExpect(status().isOk()).andExpect(content().string(hello));
     }
 
-    @WithMockUser(roles="USER")
+    @WithMockUser(roles = "USER")
     @Test
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
