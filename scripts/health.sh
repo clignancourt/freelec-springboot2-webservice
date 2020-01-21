068762428b45525744a2d3002265122216a99bab
ej <clignancourt64@gmail.com>
2020-01-21 21:22:43 +0900

    Health.sh 업데이트

--- scripts/health.sh
+++ scripts/health.sh
@@ -24,12 +24,12 @@ do
       break
   else
       echo "> Health check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
-      echo "> Health check : ${RESPONSE}"
+      echo "> Health check: ${RESPONSE}"
   fi
 
   if [ ${RETRY_COUNT} -eq 10 ]
   then
-    echo "> Health check 실패"
+    echo "> Health check 실패. "
     echo "> 엔진엑스에 연결하지 않고 배포를 종료합니다."
     exit 1
   fi
